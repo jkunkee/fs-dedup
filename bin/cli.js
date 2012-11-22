fafcbd973f37187540d60976f125f64e465cf946
Marissa Pielstick <marissapielstick@Marissa-Pielsticks-MacBook-Pro.local>
2012-11-22 13:52:46 -0700

    Hashing code written but not tested
    CLI started, problem with 'require'

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,12 @@
+#!/usr/local/bin/node
 /*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
 (function () {
   "use strict";
+  
+  var path = require('path')
+    , FsDedup = require(path.join(__dirname, '..'))
+    //, dedupEmitter = FsDedup.create()
+    , wrench = require('wrench')
+    ;
 
 }());
